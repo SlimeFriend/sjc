f1e320245d781543acfd6e0ac8004596a2c402ba
LeeSeungJu <aliceseungju@gmail.com>
2024-10-15 17:06:20 +0900

    1015 입고검사대기목록 복구

--- sjc/src/main/java/com/sjc/app/mt/service/MtlOdVO.java
+++ sjc/src/main/java/com/sjc/app/mt/service/MtlOdVO.java
@@ -7,12 +7,12 @@ import lombok.Data;
 @Data
 public class MtlOdVO {
 private String mtlOdCode; //자재발주 코드
-private Integer mtlOdQuantity; //자재발주 수량
+private int mtlOdQuantity; //자재발주 수량
 private Date mtlOdDate;//자재발주 일자
 private Date diliveryDate;//납기일
-private Integer price;//금액
+private int price;//금액
 private String mtCode;//자재 코드
 private String cpCode;//업체 코드
-private Integer userId;//사용자번호
-
+private int userId;//사용자번호
+private String mtlOdStatus;//발주 상태
 }
--- sjc/src/main/java/com/sjc/app/quality/service/impl/QualityServiceImpl.java
+++ sjc/src/main/java/com/sjc/app/quality/service/impl/QualityServiceImpl.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sjc.app.mt.service.MtlOdVO;
 import com.sjc.app.quality.mapper.QualityMapper;
-import com.sjc.app.quality.service.MtlOdVO;
 import com.sjc.app.quality.service.QualityService;
 
 @Service
