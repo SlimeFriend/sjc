64065fffc21f00f961519b05155cc37b4a3d455e
coon3300 <coon3300@gmail.com>
2024-10-16 12:38:30 +0900

    1016 기준정보 - 업체관리 조회

--- sjc/src/main/java/com/sjc/app/info/web/InfoPrdController.java
+++ sjc/src/main/java/com/sjc/app/info/web/InfoPrdController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.sjc.app.info.service.InfoPrdService;
 import com.sjc.app.info.service.InfoUserService;
 
-@Controller // Route : 사용자의 요청(endpoint)와 그에 대한 처리
-// URI + METHOD => Service => View
-//@RequiredArgsConstructor
+@Controller
 public class InfoPrdController {
-	// 해당 컨트롤러에서 제공하는 서비스
 	private InfoPrdService infoPrdService;
 	
 	@Autowired
