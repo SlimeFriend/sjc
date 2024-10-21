bc370d6176e2e9c9f902c7d53cd4a1f58a73db06
admin <admin@YD02-07>
2024-10-21 17:34:56 +0900

    1021 완료페이지 값넘기기 하는중

--- sjc/src/main/java/com/sjc/app/quality/mapper/QualityMapper.java
+++ sjc/src/main/java/com/sjc/app/quality/mapper/QualityMapper.java
@@ -3,6 +3,8 @@ package com.sjc.app.quality.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.sjc.app.mt.service.MtlOdVO;
 import com.sjc.app.quality.service.InsItemVO;
 import com.sjc.app.quality.service.InspectionVO;
@@ -28,6 +30,13 @@ public interface QualityMapper {
 	
 	// 입고검사완료페이지(임의로 만든거) - 값 입고처리 버튼 누르면 수정
 	public void updateIncoming(Map<String, Object> item);
+
+	
+	// 입고검사완료페이지 - 값 입고처리 버튼 누르면 자재발주상태(mtl_od/ status), ins(품질검사 상태) 완료로 넘기기
+	public int updateInspectionDone(InspectionVO inspectionVO);
+	public int updateMtlOdDone(InspectionVO inspectionVO);
+
+	//public int updateMtlOdDone(@Param("mtlOdStatus")String mtlOdStatus, InspectionVO inspectionVO);
 
 
 }
