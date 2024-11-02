939577211c183434903b48f731a2ef4e4cc78543
coon3300 <coon3300@gmail.com>
2024-11-03 00:01:40 +0900

    1103 1초마다 설비온도 DB 저장 스케줄러 구현.

--- sjc/src/main/java/com/sjc/app/info/service/InfoEqService.java
+++ sjc/src/main/java/com/sjc/app/info/service/InfoEqService.java
@@ -4,6 +4,7 @@ public interface InfoEqService {
     // 온도 수집
     void getTemperature();
     // 온도 랜덤 생성
-    public ChartDataVO getChartData();
-    
+    ChartDataVO getChartData();
+    // 최근 설비 로그 조회
+    ChartDataVO getEqLog();
 }
--- sjc/src/main/java/com/sjc/app/info/service/impl/InfoEqServiceImpl.java
+++ sjc/src/main/java/com/sjc/app/info/service/impl/InfoEqServiceImpl.java
@@ -6,13 +6,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sjc.app.info.mapper.InfoEqMapper;
 import com.sjc.app.info.service.ChartDataVO;
 import com.sjc.app.info.service.ChartDataVO.SeriesVO;
+import com.sjc.app.info.service.EqLogVO;
 import com.sjc.app.info.service.InfoEqService;
 @Service
 public class InfoEqServiceImpl implements InfoEqService {
+	private InfoEqMapper infoEqMapper;
+	
+	@Autowired
+	public InfoEqServiceImpl(InfoEqMapper infoEqMapper) {
+		this.infoEqMapper = infoEqMapper;
+	}
 
 	@Override
 	public void getTemperature() {
@@ -21,6 +30,7 @@ public class InfoEqServiceImpl implements InfoEqService {
 		System.out.println("--------");
 	}
 
+	// 랜덤 온도 생성
 	@Override
     public ChartDataVO getChartData() {
         ChartDataVO chartData = new ChartDataVO();
@@ -36,6 +46,8 @@ public class InfoEqServiceImpl implements InfoEqService {
         // 140~145 사이의 랜덤 값 생성
         double moldValue = Math.round((140 + Math.random() * 5) * 10.0) / 10.0;
         double mixValue = Math.round((140 + Math.random() * 5) * 10.0) / 10.0;
+//        String moldValue =  Double.toString(Math.round((140 + Math.random() * 5) * 10.0) / 10.0);
+//        String mixValue = Double.toString(Math.round((140 + Math.random() * 5) * 10.0) / 10.0);
         
         // 성형기 데이터
         SeriesVO moldSeries = new ChartDataVO.SeriesVO();
@@ -53,5 +65,44 @@ public class InfoEqServiceImpl implements InfoEqService {
         
         return chartData;
     }
+	
+	@Override
+	public ChartDataVO getEqLog() {
+
+        ChartDataVO chartData = new ChartDataVO();
+
+		List<String> categories = new ArrayList<>();
+		String currentTime = infoEqMapper.selectEqLogTime();
+		categories.add(currentTime);
+		
+		EqLogVO moldEqLogVO = new EqLogVO();
+		EqLogVO mixEqLogVO = new EqLogVO();		
+		
+		moldEqLogVO.setEqName("성형기");
+		mixEqLogVO.setEqName("배합기");
+		
+//		double moldValue = infoEqMapper.selectEqLog(moldEqLogVO);
+//		double mixValue = infoEqMapper.selectEqLog(mixEqLogVO);
+		moldEqLogVO.setEqLogData(infoEqMapper.selectEqLog(moldEqLogVO));
+		mixEqLogVO.setEqLogData(infoEqMapper.selectEqLog(mixEqLogVO));
+		
+		// 성형기 데이터
+		SeriesVO moldSeries = new ChartDataVO.SeriesVO();
+		moldSeries.setName(moldEqLogVO.getEqName());
+//		moldSeries.setData(Arrays.asList(moldEqLogVO.getEqLogData()));
+	    moldSeries.setData(Arrays.asList(Double.valueOf(moldEqLogVO.getEqLogData())));
+
+		// 혼합기 데이터
+		SeriesVO mixSeries = new ChartDataVO.SeriesVO();
+		mixSeries.setName(mixEqLogVO.getEqName());
+//		mixSeries.setData(Arrays.asList(mixEqLogVO.getEqLogData()));
+		mixSeries.setData(Arrays.asList(Double.valueOf(mixEqLogVO.getEqLogData())));
+		
+		// series 데이터 설정
+		chartData.setSeries(Arrays.asList(moldSeries, mixSeries));
+		chartData.setCategories(categories);
+		
+		return chartData;
+	}
 
 }
