8eb7ee477a5bea281de5776ee166fd8ad397b880
coon3300 <coon3300@gmail.com>
2024-11-10 15:25:57 +0900

    1110 업체관리 사이드 패널로 변경. 업체VO 추가

--- sjc/src/main/java/com/sjc/app/sales/service/CpVO.java
+++ sjc/src/main/java/com/sjc/app/sales/service/CpVO.java
@@ -1,5 +1,9 @@
 package com.sjc.app.sales.service;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -10,4 +14,7 @@ public class CpVO {
 	private String businessNo;
 	private String address;
 	private String comm;
+	private String tel;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate cpDate;
 }
