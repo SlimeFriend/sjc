5d26c3e9e78f3a76c99f94b4c10d5cb1304a58a1
coon3300 <coon3300@gmail.com>
2024-11-13 20:11:25 +0900

    1113 사용자 관리 접근 제한.

--- sjc/src/main/java/com/sjc/app/security/config/SpringSecurityConfig.java
+++ sjc/src/main/java/com/sjc/app/security/config/SpringSecurityConfig.java
@@ -31,9 +31,9 @@ public class SpringSecurityConfig {
 //				.requestMatchers("/user/**").hasRole("USER")
 				.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
 				.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+				.requestMatchers("/infoUserList").hasRole("ADMIN")  //        
 	            // Spring Boot Admin과 Actuator 엔드포인트 허용
 	            .requestMatchers("/actuator/**", "/instances/**", "/assets/**",  "/login", "/error").permitAll()
-	         
 				.anyRequest().authenticated() // 위 경로 제외 인증되면 접속 허가. 권한 필요 없음.
 		)
 		.formLogin(formlogin -> formlogin
